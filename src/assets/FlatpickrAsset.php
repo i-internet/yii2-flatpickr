4529595ef9918634bb7cc4f2b26a620556e5c629
Emp 000 <emp000@myworkaholic.com>
2024-11-01 12:56:06 +0530

    added changes

--- src/assets/FlatpickrAsset.php
+++ src/assets/FlatpickrAsset.php
@@ -8,46 +8,58 @@ use yii\web\AssetBundle;
 
 class FlatpickrAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/flatpickr/dist';
+    // Set both source paths
+    public $flatpickrPath = '@bower/flatpickr/dist';
+    public $sourcePath = '@vendor/beaten-sect0r/yii2-flatpickr/src';
+
     public $locale;
     public $plugins = [];
     public $theme;
-    public $js = [
-        'flatpickr.min.js',
+
+    public $depends = [
+        'yii\web\JqueryAsset',
     ];
-    public $css = [
-        'flatpickr.min.css',
-    ];
+
+    public function init()
+    {
+        parent::init();
+
+        $this->js = [
+            Yii::getAlias($this->flatpickrPath) . '/flatpickr.min.js',
+        ];
+        $this->css = [
+            Yii::getAlias($this->flatpickrPath) . '/flatpickr.min.css',
+        ];
+    }
 
     public function registerAssetFiles($view)
     {
         // language
         if ($this->locale !== null && $this->locale !== 'en') {
-            $this->js[] = 'l10n/' . $this->locale . '.js';
+            $this->js[] = Yii::getAlias($this->flatpickrPath) . '/l10n/' . $this->locale . '.js';
         }
 
         // plugin
         if (!empty($this->plugins) && is_array($this->plugins)) {
             if (ArrayHelper::keyExists('rangePlugin', $this->plugins)) {
-                $this->js[] = 'plugins/rangePlugin.js';
+                $this->js[] = Yii::getAlias($this->flatpickrPath) . '/plugins/rangePlugin.js';
             }
             if (ArrayHelper::keyExists('confirmDate', $this->plugins)) {
-               // $this->js[] = 'plugins/confirmDate/confirmDate.js';
-                $this->js[] = Yii::getAlias('@vendor/beaten-sect0r/yii2-flatpickr/src/js/confirmDate.js');
-
-                $this->css[] = 'plugins/confirmDate/confirmDate.css';
+                // Local confirmDate.js
+                $this->js[] = 'js/confirmDate.js';
+                $this->css[] = Yii::getAlias($this->flatpickrPath) . '/plugins/confirmDate/confirmDate.css';
             }
             if (ArrayHelper::isIn('label', $this->plugins)) {
-                $this->js[] = 'plugins/labelPlugin/labelPlugin.js';
+                $this->js[] = Yii::getAlias($this->flatpickrPath) . '/plugins/labelPlugin/labelPlugin.js';
             }
             if (ArrayHelper::keyExists('weekSelect', $this->plugins)) {
-                $this->js[] = 'plugins/weekSelect/weekSelect.js';
+                $this->js[] = Yii::getAlias($this->flatpickrPath) . '/plugins/weekSelect/weekSelect.js';
             }
         }
 
         // theme
         if (!empty($this->theme)) {
-            $this->css[] = 'themes/' . $this->theme . '.css';
+            $this->css[] = Yii::getAlias($this->flatpickrPath) . '/themes/' . $this->theme . '.css';
         }
 
         parent::registerAssetFiles($view);
